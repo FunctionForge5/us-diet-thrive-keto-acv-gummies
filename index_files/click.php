f36f37e481d4e728d92fdbdf14f0a17f827cf962
FunctionForge5 <nh332891@gmail.com>
2024-07-08 12:57:28 +0800

    Add files via upload

---
+++ index_files/click.php
@@ -0,0 +1,7 @@
+
+				var d = new Date();
+				d.setTime(d.getTime() + 3600 * 24 * 1000);
+				document.cookie = 'uclick=' + encodeURIComponent('7sb7qd6o') + '; expires='+d+'; path=/';
+				
+				var tokens = new Object();
+			 tokens.uclick = "7sb7qd6o"; 
